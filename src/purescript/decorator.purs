ed425eb188ff6d28b75ec4201e3600d0cedf4eea
Andrew Condon <andrew.condon@gmail.com>
2015-07-18 16:46:20 +0200

    fixes to folds & remove enums and tuples

--- src/purescript/decorator.purs
+++ src/purescript/decorator.purs
@@ -7,21 +7,32 @@ import Control.Monad.State.Class    -- from purescript-transformers
 import Control.Monad.Maybe.Trans    -- from purescript-transformers
 import Data.Foldable
 import Data.Traversable
-import Data.Enum
-import Data.Tuple
+--import Data.Enum
+--import Data.Tuple
 import Data.Maybe
 import Data.Monoid
 
-postIncrement :: forall s. (Enum s) => State s s
+class SimpleEnum a where
+  succ :: a -> a
+  pred :: a -> a
+
+instance intEnum :: SimpleEnum Int where
+  succ i = i + 1
+  pred i = i - 1
+
+
+{-
+postIncrement :: forall s. (SimpleEnum s) => State s s
 postIncrement = do val <- get
-                   put $ fromMaybe (succ val)
+                   put (succ val)
                    return val
 
 -- Works for any Traversable, not just trees!
-tag :: forall s t. (Enum s, Traversable t) => s -> t a -> t (Tuple a s)    -- no native tuples in Purescript!
+tag :: forall s t. (SimpleEnum s, Traversable t) => s -> t a -> t { node :: a, tag :: s }    -- no native tuples in Purescript!
 tag init tree = evalState (traverse step tree) init
     where step a = do tag <- postIncrement
-                      return (Tuple a tag)
+                      return { node: a, tag: tag}
+-}
 
 -- now let's make our own Tree with no deriving and see how that works
 data MyTree a = Empty | Leaf a | Node (MyTree a) a (MyTree a)
@@ -34,24 +45,24 @@ instance functorMyTree :: Functor MyTree where        -- no need for fmap distin
 instance foldableMyTree :: Foldable MyTree where
   foldMap f Empty = mempty
   foldMap f (Leaf x) = f x
-  foldMap f (Node l k r) = foldMap f l `append` f k `append` foldMap f r   -- append from Semigroup instead of mappend from monoid
+  foldMap f (Node l k r) = foldMap f l `append` f k `append` foldMap f r   -- append from Semigroup in PS instead of mappend from monoid in Haskell
 -- foldl & foldr required in Purescript but not in Haskell
 -- foldr :: forall a b. (a -> b -> b) -> b -> f a -> b
   foldr                  f               z    Empty = z
   foldr                  f               z   (Leaf x) = f x z
   foldr                  f               z   (Node l k r) = left
     where
-      right  = foldr  f z r     -- b
+      right  = foldr  f z r     -- yields b
       center = f k right        -- a -> b -> b 
-      left   = foldr f center l -- b
+      left   = foldr f center l -- yields b
 --  foldl :: forall a b. (b -> a -> b) -> b -> f a -> b
   foldl                  f                z    Empty = z
   foldl                  f                z   (Leaf x) = f z x
   foldl                  f                z   (Node l k r) = right
     where
-      left   = foldl f z l       -- b
+      left   = foldl f z l       -- yields b
       center = f left k          -- b -> a -> b
-      right  = foldl f center r  -- b             
+      right  = foldl f center r  -- yields b             
 
 instance traversableMyTree :: Traversable MyTree where
   traverse f Empty = pure Empty
@@ -73,4 +84,5 @@ fooMyTree = Node (Node Empty "left" Empty) "center" (Node Empty "right" Empty)
 
 main = do
   log "hello world"
-  --print $ tag 0 fooMyTree
+  --let bar = tag 0 fooMyTree
+  print "not trying to print bar yet"
